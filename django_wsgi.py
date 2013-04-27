5820938b17e5be8fc479dd6970aeecb7015fd290
daniel <daniel@daniel-OptiPlex-320.(none)>
2013-04-27 14:30:43 +0800

    new

---
+++ django_wsgi.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+ # coding: utf-8
+
+import os,sys  
+  
+if not os.path.dirname(__file__) in sys.path[:1]:
+	sys.path.insert(0, os.path.dirname(__file__))  
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'  
+  
+from django.core.handlers.wsgi import WSGIHandler  
+application = WSGIHandler()
